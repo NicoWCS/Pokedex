0474d8777663979994d317dc7a8f449e0793a369
NicoWCS <nicolas.dougere@gmail.com>
2023-06-05 10:17:56 +0200

    Added new pokemon and branch compomenent.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Bulbasaur from "./components/PokemonCard";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
-      <Bulbasaur />
+      <PokemonCard />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,26 @@
-const Bulbasaur = () => {
+const PokemonCard = () => {
+  const pokemon = pokemonList[1];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Un joli pokemon"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p> ??? </p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
 
-export default Bulbasaur;
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+export default PokemonCard;
