63425bc78ced6ca62f1f52eb52a5b7dae3d3dcce
NicoWCS <nicolas.dougere@gmail.com>
2023-06-11 18:34:41 +0200

    mise en place props

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PokemonCard = ({ pokemon }) => {
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+};
+
+export default PokemonCard;
